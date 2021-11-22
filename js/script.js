26e8b2016e073d1e48675b17bb9c94c907e74ade
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-22 14:47:30 +0100

    print mail list

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ const app = new Vue({
 
   mounted(){
 
-    for(let i = 0; i < 11; i++){
+    for(let i = 0; i < 10; i++){
 
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then((response) => {
