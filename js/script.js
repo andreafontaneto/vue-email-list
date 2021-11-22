0115e0cd616ee1a19de0372e905334e6367a4838
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-22 14:14:32 +0100

    init project

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*
+
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+** Bonus **
+1. Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+2. Predisporre una scritta “Errore” nel caso fallisse la chiamata HTTP
+
+Consigli:
+Iniziate a testare una chiamata axios direttamente da mounted.
+Analizzate bene i dati in arrivo.
+Solo in un secondo momento predisponete la logica delle 10 chiamate
+
+*/
+
+const app = new Vue({
+
+  el: '#app',
+
+  data: {
+
+  },
+
+  method: {
+
+  },
+
+  mounted(){
+
+  }
+
+})
